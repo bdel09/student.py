fbdf36c13c4471c0e100159d0bce5e760435cce7
bdel09 <bmcdelbarrio@gmail.com>
2025-08-31 10:37:07 -0400

    Create My turtle.py

---
+++ My turtle.py
@@ -0,0 +1,34 @@
+#Name: Benjamin Del Barrio
+#Email: Benjamin.delbarrio31@myhunter.cuny.edu
+
+import turtle               
+wn = turtle.Screen()        
+t = turtle.Turtle()
+t.pensize(5)
+t.shape("circle")
+t.color("red")
+t.right(90)
+t.forward(45)
+t.left(90)
+t.forward(45)
+t.left(90)
+t.forward(150)
+t.color("cyan")
+for i in range(2):
+    t.left(90)
+    t.forward(45)
+t.left(90)
+t.forward(150)
+t.color("blue")
+for i in range(2):
+    t.left(90)
+    t.forward(45)
+t.left(90)
+t.forward(150)
+t.color("green")
+for i in range(2):
+    t.left(90)
+    t.forward(45)
+t.left(90)
+t.forward(150)
+t.color("red")
